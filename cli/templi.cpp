2f46a27b1bd91a84a1afaa2e915f2f37d73e797d
RickaPrincy <rckprincy@gmail.com>
2023-12-23 23:19:21 +0300

    feat: configure command

--- cli/templi.cpp
+++ cli/templi.cpp
@@ -1,19 +1,39 @@
 #include <RCli/RCli.hpp>
 #include <Templi/TempliConfig.hpp>
 #include <Templi/Templi.hpp>
+#include <Templi/cli.hpp>
+#include <TColor/TColor.hpp>
 
 int main(int argc,const char *argv[]){
     Templi::String version = 
-        std::to_string(RCli_VERSION_MAJOR) + "." +
-        std::to_string(RCli_VERSION_MINOR) + "." +
-        std::to_string(RCli_VERSION_PATCH);
-
-    RCli::App templi("templi", version, "Generate a project easily");
+        std::to_string(Templi_VERSION_MAJOR) + "." +
+        std::to_string(Templi_VERSION_MINOR) + "." +
+        std::to_string(Templi_VERSION_PATCH);
     
+    RCli::App templi("templi", version, "Generate a project easily");
     templi.add_informations({
         {"Author", "RickaPrincy"},
         {"Github", "<https://github.com/RickaPrincy/Templi"}
     });
+    
+    Templi::String template_path, output_path;
+    Templi::VectorString ignored_paths;
+    
+    RCli::InputConfig config = RCli::InputConfig().clean(true).required(true);
+    RCli::Command configure("configure", "Configure one template to generate templi.json",[&](RCli::Command *_configure){
+        Templi::String template_path = _configure->get_option_value("template_path");
 
+        if(template_path.empty()){
+            template_path = RCli::ask_input_value(config.text("Path to the template"));
+        }
+        Templi::configure(template_path);
+        TColor::write(TColor::BLUE, "[ DONE ]: ");
+        TColor::write(TColor::YELLOW, " templi.json ");
+        TColor::write_endl(TColor::BLUE, "was generated for your template");
+    });
+
+    configure.add_option(RCli::Option("-t,--template", "Specify template path", "template_path"));
+    
+    templi.add_subcommand(configure);
     templi.run(argc, argv);
 }
---
+++ include/Templi/cli.hpp
@@ -0,0 +1,9 @@
+#ifndef __TEMPLI_CLI_FUNCTION__
+#define __TEMPLI_CLI_FUNCTION__
+
+    #include <Templi/types.hpp> 
+    #include <RCli/RCli.hpp> 
+
+    namespace Templi{
+    }
+#endif
