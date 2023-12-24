040c2fb0d2ebb938d453348149fcfce3f85425ab
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2023-12-24 23:06:15 +0300

    chore: cd publish on AUR
    * feat: create PKGBUILD
    
    * chore: add cd aur-publish

--- configure_and_build.sh
+++ configure_and_build.sh
@@ -2,7 +2,6 @@
 cmake \
     -DBUILD_GMOCK=OFF \
     -DINSTALL_GTEST=OFF \
-    -DTEMPLI_VERSION_BUILD=ON \
     -DTEMPLI_CLI_BUILD=ON \
     -DTEMPLI_TEST=ON \
     -S . -B build && cd build; make
