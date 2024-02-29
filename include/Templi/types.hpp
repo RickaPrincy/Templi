e2d4d00dec7c5932dcfd42afc8c7c57bb3cedad0
RickaPrincy <rckprincy@gmail.com>
2024-02-29 20:48:59 +0300

    chore: create class types for templi config

--- include/Templi/types.hpp
+++ include/Templi/types.hpp
@@ -4,7 +4,10 @@
 #include <map>
 #include <set>
 #include <string>
+#include <variant>
 #include <vector>
+
+#define TEMPLI_CONFIG_NAME "templi.json"
 
 namespace Templi
 {
@@ -13,4 +16,53 @@ namespace Templi
 	using VectorString = std::vector<String>;
 	using MapString = std::map<String, String>;
 	using SetString = std::set<String>;
+
+	class TempliException : public std::exception
+	{
+	private:
+		String _message;
+
+	public:
+		TempliException(const char* message) : _message(message)
+		{
+		}
+		const char* what() const noexcept override
+		{
+			return _message.c_str();
+		}
+	};	// TempliException
+
+	// all supported types of key in the templi.json["keys"]
+	enum class KeyType
+	{
+		INPUT,
+		BOOLEAN,
+		SELECT
+	};	// KeyType
+
+	class Key
+	{
+	public:
+		String _key /*key_value*/ {}, label{};
+		KeyType _type;
+		VectorString _choice{};
+		bool _required{ false }, _clean{ true };
+		std::variant<bool, String> _default;
+
+		Key(){};
+	};	// Key
+
+	class JSONConfig
+	{
+	public:
+		VectorString ignored_paths{};
+		std::vector<Key> _keys{};
+		
+        void read_config(String template_path);
+		void save_config(String template_path);
+
+		JSONConfig(){};
+		JSONConfig(String template_path){};
+	};	// JSONConfig
+
 }  // namespace Templi
---
+++ src/lib/types.cpp
@@ -0,0 +1,6 @@
+#include <Templi/types.hpp>
+
+using namespace Templi; 
+
+void Templi::JSONConfig::read_config(String template_path){
+}
