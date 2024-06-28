d18fede705285dce5258bc7800d68e1ed870009d
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2024-06-28 23:46:55 +0300

    feat: escape brackets
    * feat: escape brackets
    
    * fix: escaped value

--- tests/string/brackets_parser.cpp
+++ tests/string/brackets_parser.cpp
@@ -4,10 +4,10 @@
 
 using namespace Templi;
 
-static void test_result(String &text, String output, MapString values)
+static void test_result(String &text, String expected, MapString values)
 {
-	String result = Templi::replace_brackets_words(text, values);
-	ASSERT_EQ(result, output);
+	String actual = Templi::replace_brackets_words(text, values);
+	ASSERT_EQ(expected, actual);
 }
 
 TEST(brakets_parser, no_word)
@@ -25,10 +25,10 @@ TEST(brakets_parser, one_word)
 	// Arrange
 	MapString values = { { "one", "cool" } };
 	String text = "I have {{one}} value";
-	String output = "I have cool value";
+	String expected = "I have cool value";
 
 	// Act && Assert
-	test_result(text, output, values);
+	test_result(text, expected, values);
 }
 
 TEST(brakets_parser, one_word_at_end)
@@ -36,10 +36,10 @@ TEST(brakets_parser, one_word_at_end)
 	// Arrange
 	MapString values = { { "one", "cool" } };
 	String text = "I have {{one}}";
-	String output = "I have cool";
+	String expected = "I have cool";
 
 	// Act && Assert
-	test_result(text, output, values);
+	test_result(text, expected, values);
 }
 
 TEST(brakets_parser, one_word_at_begin)
@@ -47,10 +47,10 @@ TEST(brakets_parser, one_word_at_begin)
 	// Arrange
 	MapString values = { { "je", "cool" } };
 	String text = "{{je}} I have";
-	String output = "cool I have";
+	String expected = "cool I have";
 
 	// Act && Assert
-	test_result(text, output, values);
+	test_result(text, expected, values);
 }
 
 TEST(brakets_parser, one_word_only)
@@ -58,10 +58,10 @@ TEST(brakets_parser, one_word_only)
 	// Arrange
 	MapString values = { { "je", "cool" } };
 	String text = "{{je}}";
-	String output = "cool";
+	String expected = "cool";
 
 	// Act && Assert
-	test_result(text, output, values);
+	test_result(text, expected, values);
 }
 
 TEST(brakets_parser, two_words)
@@ -69,30 +69,41 @@ TEST(brakets_parser, two_words)
 	// Arrange
 	MapString values = { { "je", "cool" }, { "happy", "noo" } };
 	String text = "{{je}} je ricka princy {{happy}} lorem";
-	String output = "cool je ricka princy noo lorem";
+	String expected = "cool je ricka princy noo lorem";
 
 	// Act && Assert
-	test_result(text, output, values);
+	test_result(text, expected, values);
 }
 
 TEST(brakets_parser, many_words)
 {
 	// Arrange
 	String text = "je{{cool}} I'm happy{{lol}},!no\\ motion {{number1}}, things, {{nice}}";
-	String output = "jeje I'm happylolo,!no\\ motion {{number1}}, things, nice";
+	String expected = "jeje I'm happylolo,!no\\ motion {{number1}}, things, nice";
 	MapString values = { { "cool", "je" }, { "lol", "lolo" }, { "nice", "nice" } };
 
 	// Act && Assert
-	test_result(text, output, values);
+	test_result(text, expected, values);
 }
 
 TEST(brakets_parser, with_no_close)
 {
 	// Arrange
 	String text = "je{{cool{{nice}}, {{je";
-	String output = "jeVALUE, {{je";
+	String expected = "jeVALUE, {{je";
 	MapString values = { { "cool{{nice", "VALUE" } };
 
 	// Act && Assert
-	test_result(text, output, values);
+	test_result(text, expected, values);
+}
+
+TEST(brakets_parser, with_escaped_bracket)
+{
+	// Arrange
+	String text = "je\\{{escaped{{input}}";
+	String expected = "je{{escapedvalue";
+	MapString values = { { "input", "value" } };
+
+	// Act && Assert
+	test_result(text, expected, values);
 }
