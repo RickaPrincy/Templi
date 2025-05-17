2bac0ddc5f6415ec818834db24f672b8a81f126e
RickaPrincy <rckprincy@gmail.com>
2025-05-17 08:17:30 +0300

    release: libtempli@4.1.1 - templi_cli@4.1.1

--- include/Templi/TempliConfig.hpp
+++ include/Templi/TempliConfig.hpp
@@ -1,6 +1,6 @@
 // clang-format off
-#define Templi_VERSION_MAJOR
-#define Templi_VERSION_MINOR
-#define Templi_VERSION_PATCH
+#define Templi_VERSION_MAJOR 
+#define Templi_VERSION_MINOR 
+#define Templi_VERSION_PATCH 
 #define Templi_VERSION "4.1.1"
-// clang-format on 
+// clang-format on
